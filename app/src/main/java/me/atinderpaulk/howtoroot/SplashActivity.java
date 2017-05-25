2ad0570ce755c584261930d47a77c7f7e77a6cb2
Atinderpaul Kanwar <ap_kanwar@outlook.com>
2017-05-25 16:30:12 -0400

    Changed splash screen code

--- app/src/main/java/me/atinderpaulk/howtoroot/SplashActivity.java
+++ app/src/main/java/me/atinderpaulk/howtoroot/SplashActivity.java
@@ -14,20 +14,9 @@ public class SplashActivity extends Activity {
 
         int SPLASH_TIME_OUT = 3000;
         new Handler().postDelayed(new Runnable() {
-
-            /*
-             * Showing splash screen with a timer. This will be useful when you
-             * want to show case your app logo / company
-             */
-
             @Override
             public void run() {
-                // This method will be executed once the timer is over
-                // Start your app main activity
-                Intent i = new Intent(SplashActivity.this, MainActivity.class);
-                startActivity(i);
-
-                // close this activity
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
                 finish();
             }
         }, SPLASH_TIME_OUT);
